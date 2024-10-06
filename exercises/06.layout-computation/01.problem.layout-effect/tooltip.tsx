c29683e29ff036e207f98dab3137789a49f12e95
Jacobo Jaramillo <Jacobo.Jaramilo-Molina@kuka.com>
2024-10-06 22:11:12 +0100

    section 6 done

--- exercises/06.layout-computation/01.problem.layout-effect/tooltip.tsx
+++ exercises/06.layout-computation/01.problem.layout-effect/tooltip.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useLayoutEffect, useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 
 type Position = {
@@ -6,6 +6,27 @@ type Position = {
 	top: number
 	right: number
 	bottom: number
+}
+
+function useWindowSize() {
+	const [windowSize, setWindowSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight,
+	})
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			})
+		}
+
+		window.addEventListener('resize', handleResize)
+		return () => window.removeEventListener('resize', handleResize)
+	}, [])
+
+	return windowSize
 }
 
 export default function Tooltip({
@@ -17,19 +38,26 @@ export default function Tooltip({
 }) {
 	const ref = useRef<HTMLDivElement | null>(null)
 	const [tooltipHeight, setTooltipHeight] = useState(0)
+	const [tooltipWidth, setTooltipWidth] = useState(0)
+	const windowSize = useWindowSize()
 
 	// 🐨 change this to useLayoutEffect to ensure it runs synchronously after the
 	// DOM has been updated so the user doesn't see the tooltip jump around.
-	useEffect(() => {
+	useLayoutEffect(() => {
 		const rect = ref.current?.getBoundingClientRect()
 		if (!rect) return
-		const { height } = rect
+		const { height, width } = rect
 		setTooltipHeight(height)
+		setTooltipWidth(width)
 	}, [])
+
+	// console.log(tooltipHeight)
 
 	let tooltipX = 0
 	let tooltipY = 0
 	if (targetRect !== null) {
+		console.log(tooltipWidth + targetRect.left)
+		console.log(windowSize.width)
 		tooltipX = targetRect.left
 		tooltipY = targetRect.top - tooltipHeight
 		if (tooltipY < 0) {
@@ -38,6 +66,11 @@ export default function Tooltip({
 
 		tooltipX += window.scrollX
 		tooltipY += window.scrollY
+	}
+
+	// If the tooltip overflows to the right, move it to the left
+	if (targetRect && tooltipWidth + targetRect.left > windowSize.width) {
+		tooltipX = windowSize.width - tooltipWidth - 10
 	}
 
 	// This artificially slows down rendering
